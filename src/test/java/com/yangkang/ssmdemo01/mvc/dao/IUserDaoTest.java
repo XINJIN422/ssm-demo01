e6d12b1ab9c125deb09dd9c340604a8d5fc1247a
yangkang <940166812@qq.com>
2018-08-09 15:47:50 +0800

    统一dao封装,不用再写mapper接口

--- src/test/java/com/yangkang/ssmdemo01/mvc/dao/IUserDaoTest.java
+++ src/test/java/com/yangkang/ssmdemo01/mvc/dao/IUserDaoTest.java
@@ -1,22 +1,29 @@
 package com.yangkang.ssmdemo01.mvc.dao;
 
 import com.yangkang.ssmdemo01.mvc.entity.User;
+import com.yangkang.ssmdemo01.mvc.service.IUserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration({"classpath:spring/spring-mybatis.xml"})    配置了web容器启动监听器并指定位置后就不需要了
+@ContextConfiguration({"classpath:spring/spring-config.xml"})
 public class IUserDaoTest {
 
     @Autowired
-    private IUserDao dao;
+    private IUserDao userDao;
+
+    @Autowired
+    private IUserService userService;
 
     @Test
     public void testSelectUser() throws Exception {
-        long id = 1;
-        User user = dao.selectUser(id);
+//        long id = 1;
+//        User user = userDao.selectUser(id);
+        String id = "1";
+        User user = userService.selectUser2(id);
         System.out.println(user.getUsername());
     }
 }
