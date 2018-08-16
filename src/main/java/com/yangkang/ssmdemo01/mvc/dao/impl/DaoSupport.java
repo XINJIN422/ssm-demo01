05ca399268978c147b8c1fed76ecbdc386967424
yangkang <940166812@qq.com>
2018-08-16 20:31:45 +0800

    尝试dbcp,druid两种数据源连接方式

--- src/main/java/com/yangkang/ssmdemo01/mvc/dao/impl/DaoSupport.java
+++ src/main/java/com/yangkang/ssmdemo01/mvc/dao/impl/DaoSupport.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Repository
 public class DaoSupport implements Dao {
 
-    @Resource(name = "sqlSessionTemplate")
+    @Resource(name = "sqlSessionTemplate5")
     private SqlSessionTemplate sqlSessionTemplate;
 
     /**
